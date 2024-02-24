6dd2c310f5827602e8ce789d9634720a2d74b5c3
MrKuru <melihcaan.99@gmail.com>
2024-02-24 21:57:42 +0300

    init

---
+++ src/test/java/com/melh/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melh.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
